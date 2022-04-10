f7e0453646e8d51221cff9f192fe4cb8d07100f8
Sergey Yakovlev <sergeyyak@gmail.com>
2022-04-10 12:57:21 -0600

    added swapping nodes problem

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use leet_code::swapping_nodes::*;
+fn main() {
+    let l = from_vec(vec![1, 2]);
+    // let l = from_vec(vec![1, 2, 3, 4, 5, 6]);
+    let res = swap_nodes(l, 1);
+    println!("the list is {:?}", res);
+}
